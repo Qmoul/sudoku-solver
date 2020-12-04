44aafb6cc12754940cff57bb136384f3211db2c5
qmoul <qmouls@gmail.com>
2020-12-04 17:39:24 +0100

    created empty solver constructor

--- src/main/java/org/benga/cc/sudoku/Solver/sudokuSolver/SudokuSolver.java
+++ src/main/java/org/benga/cc/sudoku/Solver/sudokuSolver/SudokuSolver.java
@@ -3,6 +3,13 @@ package org.benga.cc.sudoku.Solver.sudokuSolver;
 public class SudokuSolver {
     private int[][] unsolvedMatrix;
     private int[][] solvedMatrix;
+    private boolean finished = false;
+
+    public void Solver(){}
+
+    public void setUnsolvedMatrix(int[][] unsolvedMatrix){
+        this.unsolvedMatrix = unsolvedMatrix;
+    }
 
     public void Solver(int[][] unsolvedMatrix){
         this.unsolvedMatrix = unsolvedMatrix;
@@ -14,9 +21,22 @@ public class SudokuSolver {
             }
         }
     }
-    public void solve(int indexX, int indexY){
-        if(solvedMatrix[indexX][indexY] == 0){
-
+    public boolean solve(int indexX, int indexY, int depth, int startingNumber){
+        if (depth == 81) return true;
+        if (finished) return true;
+        if(++indexX == 9){indexX = 0;}
+        if(++indexY == 9){indexY = 0;}
+        if(solvedMatrix[indexX][indexY] != 0){
+            return solve(indexX, indexY, depth, startingNumber);
         }
+        for(int i = 1; i < 10; ++i){
+            if(++startingNumber == 10) {startingNumber = 1;}
+            if
+        }
+    return false;
+    }
+    public boolean validator(){
+
+        return true;
     }
 }
